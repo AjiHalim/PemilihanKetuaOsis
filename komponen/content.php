a3fb5eeb85962298f0788c5b445c4f2fa8897a02
AjiHalim <132763513+AjiHalim@users.noreply.github.com>
2023-05-26 21:03:35 +0700

    update page content
    menammbahkan keterangan di halaman depan calon

--- komponen/content.php
+++ komponen/content.php
@@ -8,11 +8,11 @@
     <!-- Main content -->
       <section class="content">
           <div class="container-fluid">
-            	<!-- <div class="card bg-info mt-4" style="padding: 10px">
+            	<div class="card bg-info mt-4" style="padding: 10px">
                 <p>
                   <b>Mohon Maaf Untuk Saat Ini Dalam PengUpload-an File Foto atau Video Belum Bisa Menggunakan Aplikasi Pemilos <br/> Anda Dapat Menghubungi Team Developer Dari Aplikasi Pemilos Melalui Email <i style="font-size: 20px"> timdevpemilossmkbatik@gmail.com</i> Untuk Mengupload File dan Untuk Mengadukan Jika Ada Error Di Aplikasi Terima Kasih</b>
                 </p>
-              </div> -->
+              </div>
           </div>
       </section>
     <!-- /.content -->
